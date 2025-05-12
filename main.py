2203cfff0295dac50533438e4648a8f94133f4da
budana12 <sirowabudana@gmail.com>
2025-05-12 17:22:08 +0530

    main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def show_random_emojis(message):
 OWNER_ID = 6520378417 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = []
+SUDO_USERS = [6520378417]
 
 AUTH_CHANNEL = -2631081956
 
